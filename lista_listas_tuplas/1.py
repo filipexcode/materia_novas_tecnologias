2f0ad4c6b1a6549df3b84bde531701d8bed848e5
filipe <filipesilva1503code@gmail.com>
2024-09-23 19:35:30 -0300

    questao 1

--- lista_listas_tuplas/1.py
+++ lista_listas_tuplas/1.py
@@ -5,5 +5,17 @@ lista = [12, -2, 4, 8, 29, 45, 78, 36, -17, 2, 12, 8,
 media = sum(lista) / len(lista)
 
 print(max(lista))
+
 print(min(lista))
+
+numeros_pares = [num for num in lista if num % 2 == 0]
+print(numeros_pares)
+
+primeiro_elemento = lista[0]
+ocorrencias_primeiro = lista.count(primeiro_elemento)
+print(ocorrencias_primeiro)
+
 print(media)
+
+soma_negativos = sum(num for num in lista if num < 0)
+print(soma_negativos)
